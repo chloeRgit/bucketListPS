0135d38c272603b262a1c9df350b870da30fa229
chloeRgit <chloe.rondeau2020@campus-eni.fr>
2021-09-21 09:34:15 +0200

    Initial commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MainController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('main/index.html.twig', [
+            'controller_name' => 'MainController',
+        ]);
+    }
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact(): Response
+    {
+        $tab["message"]="Contact";
+        return $this->json($tab);
+    }
+}
