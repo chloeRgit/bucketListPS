a34d7cffe64b0fb051aae9637877f2261c1de686
chloeRgit <chloe.rondeau2020@campus-eni.fr>
2021-09-23 14:03:31 +0200

    suppresion

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -23,6 +23,7 @@ class MainController extends AbstractController
         return $this->render('main/home.html.twig', [
             'controller_name' => 'home',
             'wishes'=>$wishes,
+            'success_delete'=>'null',
 
         ]);
     }
@@ -88,18 +89,33 @@ class MainController extends AbstractController
     public function wishUpdate(Wish $wish, Request $request): Response
     {
 
-        $formWish=$this->createForm(WishType::class, $wish);
+        $formWish = $this->createForm(WishType::class, $wish);
         $formWish->handleRequest($request);
-        if($formWish->isSubmitted()){
-            $em=$this->getDoctrine()->getManager();
+        if ($formWish->isSubmitted()) {
+            $em = $this->getDoctrine()->getManager();
             $em->flush();
             return $this->render('main/wishes.html.twig', [
                 'wish' => $wish,
-                'success_ajout'=>'null']);
+                'success_ajout' => 'null']);
         }
         return $this->render('main/wish_update.html.twig', [
             'formWish' => $formWish->createView(),
-            'success_ajout'=>'null',
+            'success_ajout' => 'null',
+        ]);
+    }
+        /**
+         * @Route("/wish_delete/{id}", name="wish_delete")
+         */
+        public function wishdelete(Wish $wish, WishRepository $repo): Response
+    {
+
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($wish);
+        $em->flush();
+        $wishes=$repo->findBy(array('isPublished' =>1), array('dateCreated' => 'DESC'));
+        return $this->render('main/home.html.twig', [
+            'success_delete'=>'delete',
+            'wishes'=>$wishes,
         ]);
 
     }
